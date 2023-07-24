8cf66c22a8312052b3c798c5c06011ff55a55b49
overflowDev <adeyemiadetomiwa.dev@gmail.com>
2023-07-24 14:42:14 +0100

    sosa ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Blog from "./components/Blog"
+// import Blog from "./components/Blog"
 import Download from "./components/Download"
 import Hero from "./components/Hero"
 import Interface from "./components/Interface"
