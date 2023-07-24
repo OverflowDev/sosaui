39cd584e03bda81ecd07411dfb1d039217556e4b
overflowDev <adeyemiadetomiwa.dev@gmail.com>
2023-07-24 14:53:25 +0100

    sosa ui

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -3,7 +3,7 @@ import {useState} from 'react'
 function Navbar() {
 
     
-    const [isOPen, setIsOPen] = useState(false)
+    const [isOPen, setIsOPen] = useState<boolean>(false)
 
     const menu = () => {
         setIsOPen(!isOPen)
@@ -25,9 +25,7 @@ function Navbar() {
                     <div className="lg:hidden">
                         <button onClick={menu}>
                             {!isOPen ? (
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
-                                </svg>
+                                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" className=" text-white text-[32px]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M464 256H48a48 48 0 0 0 0 96h416a48 48 0 0 0 0-96zm16 128H32a16 16 0 0 0-16 16v16a64 64 0 0 0 64 64h352a64 64 0 0 0 64-64v-16a16 16 0 0 0-16-16zM58.64 224h394.72c34.57 0 54.62-43.9 34.82-75.88C448 83.2 359.55 32.1 256 32c-103.54.1-192 51.2-232.18 116.11C4 180.09 24.07 224 58.64 224zM384 112a16 16 0 1 1-16 16 16 16 0 0 1 16-16zM256 80a16 16 0 1 1-16 16 16 16 0 0 1 16-16zm-128 32a16 16 0 1 1-16 16 16 16 0 0 1 16-16z"></path></svg>
                             ) : (
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
