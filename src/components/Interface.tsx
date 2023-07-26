8a4d4aa564e47a9b7f1c42ac4ba53a2d93d25d16
overflowDev <adeyemiadetomiwa.dev@gmail.com>
2023-07-26 01:26:39 +0100

    sosa ui

--- src/components/Interface.tsx
+++ src/components/Interface.tsx
@@ -9,15 +9,15 @@ function Interface() {
                 <div className='flex-col'>
                     <div className='flex items-center'>
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" className="mr-3 text-[green]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M699 353h-46.9c-10.2 0-19.9 4.9-25.9 13.3L469 584.3l-71.2-98.8c-6-8.3-15.6-13.3-25.9-13.3H325c-6.5 0-10.3 7.4-6.5 12.7l124.6 172.8a31.8 31.8 0 0 0 51.7 0l210.6-292c3.9-5.3.1-12.7-6.4-12.7z"></path><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path></svg>
-                        <h2 className='font-semibold tracking-wider'>Installation Guide</h2>
+                        <h2 className='font-normal tracking-wider'>Installation Guide</h2>
                     </div>
                     <div className='flex items-center'>
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" className="mr-3 text-[green]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M699 353h-46.9c-10.2 0-19.9 4.9-25.9 13.3L469 584.3l-71.2-98.8c-6-8.3-15.6-13.3-25.9-13.3H325c-6.5 0-10.3 7.4-6.5 12.7l124.6 172.8a31.8 31.8 0 0 0 51.7 0l210.6-292c3.9-5.3.1-12.7-6.4-12.7z"></path><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path></svg>
-                        <h2 className='font-semibold tracking-wider'>Easy Setup process</h2>
+                        <h2 className='font-normal tracking-wider'>Easy Setup process</h2>
                     </div>
                     <div className='flex items-center'>
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" className="mr-3 text-[green]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M699 353h-46.9c-10.2 0-19.9 4.9-25.9 13.3L469 584.3l-71.2-98.8c-6-8.3-15.6-13.3-25.9-13.3H325c-6.5 0-10.3 7.4-6.5 12.7l124.6 172.8a31.8 31.8 0 0 0 51.7 0l210.6-292c3.9-5.3.1-12.7-6.4-12.7z"></path><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path></svg>
-                        <h2 className='font-semibold tracking-wider'>Live call support</h2>
+                        <h2 className='font-normal tracking-wider'>Live call support</h2>
                     </div>
                 </div>
                 <button className='mt-8 w-48 px-2 py-3 rounded-full border border-sosa hover:bg-sosa text-sosa hover:text-white'>View More</button>
@@ -27,12 +27,12 @@ function Interface() {
                 <img 
                     src="https://saas-ui.netlify.app/static/media/27.6e9d1c8838482cf8b54d.png" 
                     alt="interface" 
-                    className='object-cover mr-[-50px]  w-2/4'
+                    className='object-cover md:mr-[-50px] mr-[-150px] md:w-2/4 w-3/4'
                 />
                 <img 
                     src="https://saas-ui.netlify.app/static/media/26.7e79ef20aca819a3abcb.png" 
                     alt="interface" 
-                    className='object-cover w-2/4'
+                    className='object-cover md:w-2/4 w-3/4'
                 />
             </div>
         </div>
@@ -47,25 +47,25 @@ function Interface() {
                 <img 
                     src="https://saas-ui.netlify.app/static/media/28.4e4f0fc2c819d4c71a9a.png" 
                     alt="interface" 
-                    className='object-cover absolute top-48'
+                    className='object-cover absolute md:top-48 top-56 md:right-36 right-24 h-72'
                 />
             </div>
 
-            <div className='md:ml-24 mt-4'>
+            <div className='md:ml-24 mt-12'>
                 <h2 className="text-[28px] font-bold leading-[40px] text-sosaDark ">Super clean user <br /> interface for easier use.</h2> 
                 <p className="text-[16px] py-5 leading-7 max-w-md">Lost the plot cracking goal give us a bell is bog horse play knackered lemon squeezy, cup of char cack bleeder matie boy he lost his bottle.!</p>
                 <div className='flex-col'>
                     <div className='flex items-center'>
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" className="mr-3 text-[green]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M699 353h-46.9c-10.2 0-19.9 4.9-25.9 13.3L469 584.3l-71.2-98.8c-6-8.3-15.6-13.3-25.9-13.3H325c-6.5 0-10.3 7.4-6.5 12.7l124.6 172.8a31.8 31.8 0 0 0 51.7 0l210.6-292c3.9-5.3.1-12.7-6.4-12.7z"></path><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path></svg>
-                        <h2 className='font-semibold tracking-wider'>Secured Cloud</h2>
+                        <h2 className='font-normal tracking-wider'>Secured Cloud</h2>
                     </div>
                     <div className='flex items-center'>
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" className="mr-3 text-[green]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M699 353h-46.9c-10.2 0-19.9 4.9-25.9 13.3L469 584.3l-71.2-98.8c-6-8.3-15.6-13.3-25.9-13.3H325c-6.5 0-10.3 7.4-6.5 12.7l124.6 172.8a31.8 31.8 0 0 0 51.7 0l210.6-292c3.9-5.3.1-12.7-6.4-12.7z"></path><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path></svg>
-                        <h2 className='font-semibold tracking-wider'>Implement Yourself</h2>
+                        <h2 className='font-normal tracking-wider'>Implement Yourself</h2>
                     </div>
                     <div className='flex items-center'>
                         <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" className="mr-3 text-[green]" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M699 353h-46.9c-10.2 0-19.9 4.9-25.9 13.3L469 584.3l-71.2-98.8c-6-8.3-15.6-13.3-25.9-13.3H325c-6.5 0-10.3 7.4-6.5 12.7l124.6 172.8a31.8 31.8 0 0 0 51.7 0l210.6-292c3.9-5.3.1-12.7-6.4-12.7z"></path><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path></svg>
-                        <h2 className='font-semibold tracking-wider'>Works in Web</h2>
+                        <h2 className='font-normal tracking-wider'>Works in Web</h2>
                     </div>
                 </div>
                 <button className='mt-8 w-48 px-2 py-3 rounded-full border border-sosa hover:bg-sosa text-sosa hover:text-white'>Discover More</button>
